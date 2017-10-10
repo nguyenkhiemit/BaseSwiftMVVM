4143eaa1324951a32cd200fa8879d67498bccefa
KhiemND <nguyenkhiem010489@gmail.com>
2017-10-11 05:58:14 +0700

    **** Add Designable button

--- IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/DesignableButton.swift
+++ IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/DesignableButton.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
+enum DesignableButtonStyle: Int {
+    case Regular = 0
+    case Light
+    case Dark
+    case Fancy
+    case Alert
+}
+
 @IBDesignable
 class DesignableButton: UIButton {
-    @IBInspectable public var cornerRadius: CGFloat = 0.0 {
+    
+    @IBInspectable var buttonStyle: DesignableButtonStyle = .Regular {
+        didSet {
+            buttonStyleNumber = buttonType.rawValue
+        }
+    }
+    
+    @IBInspectable var buttonStyleNumber: Int = 0 {
+        didSet {
+            switch buttonStyleNumber {
+            case DesignableButtonStyle.Light.rawValue:
+                tintColor = UIColor.white
+                backgroundColor = UIColor.lightGray
+                break
+            case DesignableButtonStyle.Dark.rawValue:
+                tintColor = UIColor.white
+                backgroundColor = UIColor.darkGray
+                break
+            case DesignableButtonStyle.Fancy.rawValue:
+                tintColor = UIColor.white
+                backgroundColor = UIColor.purple
+                break
+            case DesignableButtonStyle.Alert.rawValue:
+                tintColor = UIColor.white
+                backgroundColor = UIColor.red
+                break
+            default:
+                //default Regular
+                tintColor = UIColor.white
+                backgroundColor = UIColor.black
+            }
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
         didSet {
             if cornerRadius != 0 {
                 self.layer.cornerRadius = cornerRadius
             }
+        }
+    }
+    
+    @IBInspectable var verticalPadding: CGFloat = 0 {
+        didSet {
+            contentEdgeInsets = UIEdgeInsetsMake(verticalPadding, horizontalPadding, verticalPadding, horizontalPadding)
+        }
+    }
+    
+    @IBInspectable var horizontalPadding: CGFloat = 0 {
+        didSet {
+            contentEdgeInsets = UIEdgeInsetsMake(verticalPadding, horizontalPadding, verticalPadding, horizontalPadding)
         }
     }
 }
--- IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/DesignableView.swift
+++ IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/DesignableView.swift
@@ -18,4 +18,6 @@ class DesignableView: UIView {
             }
         }
     }
+    
+    
 }
