490d6ceecabb8494acab0014e17f29f4df92c499
Khiem Nguyen <khiem.d.nguyen@vntrip.vn>
2017-10-11 17:55:59 +0700

    add loading view

---
+++ IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/Loading.swift
@@ -0,0 +1,20 @@
+//
+//  Loading.swift
+//  IOSBaseMVVMC
+//
+//  Created by Nguyen on 10/11/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
+
+class Loading {
+    
+    static func show() {
+        
+    }
+    
+    class func hide() {
+        
+    }
+}
