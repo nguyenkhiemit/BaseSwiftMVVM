e9556b7e49b8a232895ccbc09c2df7d21e63af4c
Khiem Nguyen <khiem.d.nguyen@vntrip.vn>
2017-10-11 16:51:09 +0700

    add designable textfield

--- IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/DesignableTextField.swift
+++ IOSBaseMVVMC/IOSBaseMVVMC/Base/View/CustomView/DesignableTextField.swift
@@ -11,10 +11,24 @@ import UIKit
 @IBDesignable
 class DesignableTextField: UITextField {
     
-    @IBInspectable public var cornerRadius: CGFloat = 0.0 {
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
         didSet {
             if cornerRadius != 0 {
                 self.layer.cornerRadius = cornerRadius
+            }
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            if borderWidth != 0 {
+                self.layer.borderWidth = borderWidth
             }
         }
     }
@@ -25,29 +39,62 @@ class DesignableTextField: UITextField {
         }
     }
     
-    @IBInspectable var leftPadding: CGFloat = 0.0 {
+    @IBInspectable var rightImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var paddingImageToBorder: CGFloat = 0.0 {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var widthImage: CGFloat = 0.0 {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var heightImage: CGFloat = 0.0 {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var paddingImageToTitle: CGFloat = 0.0 {
         didSet {
             updateView()
         }
     }
     
     func updateView() {
-        if let image = leftImage {
+        if let leftImage = leftImage {
             leftViewMode = .always
-            let imageView = UIImageView(frame: CGRect(x: leftPadding, y: 0, width: 20, height: 20))
-            imageView.image = image
-            imageView.tintColor = tintColor
-            var width = leftPadding + 20
+            let imageView = UIImageView(frame: CGRect(x: paddingImageToBorder, y: 0, width: widthImage, height: heightImage))
+            imageView.image = leftImage
+            var width = paddingImageToBorder + widthImage
             if borderStyle == UITextBorderStyle.none || borderStyle == UITextBorderStyle.line {
-                width = width + 5
+                width = width + paddingImageToTitle
             }
-            let view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 20))
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: heightImage))
             view.addSubview(imageView)
-            leftView = imageView
-        } else {
-            // image is nil
+            leftView = view
         }
         
+        if let rightImage = rightImage {
+            rightViewMode = .always
+            let imageView = UIImageView(frame: CGRect(x: paddingImageToTitle, y: 0, width: widthImage, height: heightImage))
+            imageView.image = rightImage
+            var width = paddingImageToTitle + widthImage
+            if borderStyle == UITextBorderStyle.none || borderStyle == UITextBorderStyle.line {
+                width = width + paddingImageToBorder
+            }
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: heightImage))
+            view.addSubview(imageView)
+            rightView = view
+        }
         attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: [NSForegroundColorAttributeName: tintColor])
     }
 }
--- IOSBaseMVVMC/IOSBaseMVVMC/Menu/View/MenuViewController.swift
+++ IOSBaseMVVMC/IOSBaseMVVMC/Menu/View/MenuViewController.swift
@@ -1,4 +1,4 @@
- //
+//
 //  MenuViewController.swift
 //  IOSBaseMVVMC
 //
